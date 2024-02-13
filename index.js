02be91c49d87212d2333f5a2194d645c16b80503
YxngXi1 <147933670+YxngXi1@users.noreply.github.com>
2024-02-13 13:40:50 -0500

    Change 'no' options

---
+++ index.js
@@ -0,0 +1,35 @@
+var element = document.getElementById("No");
+
+let texts = ["R u sure", "Pls", "100% sure?", "Don't do this :(", "I'll pay you", "Pls", "Maybe press the other one", "Dang I don't have enough prompts", "Please press the other one", "Ideas running low", "I want you :(", "One more chance", "last chance", "You thought", "Jk this is last chance", "Plsplspslpsl", "I'll be very sad", "pls say yes", "I'm begging", ":(((", "have mercy", "god will bless u", "PLEASE", "DDDDDDDDD:", "👺", "pwease pookie bear", "be my fwend", "😩", "or else(?)", "peewoop", "xoxo gossip gurl", "ill buy you shawarma deary", "i got $$$", "my mom will love you", "ill give u the lasT PIECE OF MY FRIED CHICKEN", "niacinamide", "nitroglycerin", "wow",, "you must rlly hate me", "i promise we'll be good together"];
+let index = 0;
+
+document.getElementById('theButton').addEventListener('click', function() {
+    this.innerHTML = texts[index];
+    index = (index+1) % texts.length; // Cycle back to 0 when reaching the end
+});
+
+// The event listener
+element.addEventListener("mousemove", function(){
+    // Debugging
+    console.log("it worked!!!")
+
+    // Change the text of the button inside the div
+    document.getElementById('theButton').textContent = texts[index];
+    index = (index + 1) % texts.length;
+
+    // The button movement section
+    document.body.appendChild(element);
+    var xCoordinate = Math.random() * 1300;
+    var yCoordinate = Math.random() * 600;
+    // console.log(xCoordinate)
+    // console.log(yCoordinate)
+    element.style.left = xCoordinate + "px";
+    element.style.top = yCoordinate + "px";
+});
+
+// Testing for coordinates
+// document.addEventListener('click', function(event) {
+//     var x = event.clientX;
+//     var y = event.clientY;
+//     console.log('X: ' + x + ', Y: ' + y);
+// });
